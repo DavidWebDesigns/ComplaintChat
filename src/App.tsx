ff9a00e8e986910c5428af0168322a6ba0e5dbbe
davidwebdesigns <daeperezh@gmail.com>
2024-02-19 22:02:21 -0500

    builded and subs updated

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,7 @@ function App() {
   return (
     <>
     <div id="chat_container">
-      <iframe src="https://app.stammer.ai/es/chatbot/embed/b4bfbbfe-5447-4bd9-9d80-c0bc2e74404f" width="100%" height="100vh" ></iframe>
-    </div>
+    <iframe src="https://app.stammer.ai/es/chatbot/embed/05b7069b-1c7d-46d2-9986-7a504df24372" width="100%" height="600" frameborder="0" ></iframe>    </div>
     </>
   )
 }
