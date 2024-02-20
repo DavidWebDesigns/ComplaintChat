26df083bd765ef5f690665ee91f06c99abec3d7c
davidwebdesigns <daeperezh@gmail.com>
2024-02-19 22:11:18 -0500

    typo fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
   return (
     <>
     <div id="chat_container">
-    <iframe src="https://app.stammer.ai/es/chatbot/embed/05b7069b-1c7d-46d2-9986-7a504df24372" width="100%" height="600" frameborder="0" ></iframe>    </div>
+    <iframe src="https://app.stammer.ai/es/chatbot/embed/05b7069b-1c7d-46d2-9986-7a504df24372" width="100%" height="600" frameBorder="0" ></iframe>
+    </div>
     </>
   )
 }
